57199374b0d3fb5c922712f3f8364f7219db6729
Anand B <banand@live.com>
2019-03-06 19:48:29 +0100

    fixing #11

--- src/main/index.js
+++ src/main/index.js
@@ -2,12 +2,16 @@
 
 import { app, BrowserWindow } from 'electron'
 
+let icon
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  icon = require('path').join(__dirname, '/static', '/256x256.png').replace(/\\/g, '\\\\')
+} else {
+  icon = require('path').join(__dirname, '..', '..', 'build', 'icons', '256x256.png')
 }
 
 let mainWindow
@@ -19,7 +23,9 @@ function createWindow () {
   /**
    * Initial window options
    */
+  console.log(require('path').join(__dirname, '..', '..', 'build', 'icons', '256x256.png'))
   mainWindow = new BrowserWindow({
+    icon: icon,
     height: 563,
     useContentSize: true,
     width: 1000
