549e6c2c71f9b94a45b1b3a28a56c082424f0153
Anand B <banand@live.com>
2019-03-20 17:22:22 +0100

    Fixing #14

--- static/scripts/sim/out.sh
+++ static/scripts/sim/out.sh
@@ -1,1 +1,1 @@
-docker exec robogen_sim /bin/bash -c "./robogen-file-viewer ../examples/$1 ../examples/$2 --output ../examples/$3 --overwrite"
+docker exec robogen_sim /bin/bash -c "./robogen-file-viewer ../examples/$1 ../examples/$2 --output ../examples/$3 --overwrite --pause"
